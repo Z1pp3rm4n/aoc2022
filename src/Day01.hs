5729511c30344eccd64c4fc2dce34abb6ecdc537
victor <vinh.ntuan@gmail.com>
2022-12-06 16:10:02 +0100

    finished Day 1

---
+++ src/Day01.hs
@@ -0,0 +1,27 @@
+module Day01 where
+
+import Tools
+import Data.List.Split (split)
+import Data.List.Split.Internals (splitOn)
+import Data.List (sort, sortBy)
+
+main = do
+    calories <- caloriesPerElf <$> getInput "01"
+    print (p1 calories)
+    print (p2 calories)
+
+
+p1 :: [Int] -> Int
+p1 = maximum 
+
+p2 :: [Int] -> Int
+p2 = sum . take 3 . descSort   
+    where 
+        descSort = sortBy (flip compare) 
+
+caloriesPerElf :: String -> [Int]
+caloriesPerElf = map sum . snacksPerElf
+
+snacksPerElf :: String -> [[Int]]
+snacksPerElf =  map (map read . lines) . splitOn "\n\n"
+
